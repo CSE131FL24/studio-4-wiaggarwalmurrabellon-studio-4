c3b062eea82d1a0836c5cb00c4095dd505ccf4fa
sarahaggarwal <sarahaggarwal@localhost>
2024-10-02 17:13:18 -0500

    Submission

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,21 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		Color washu = new Color(212,27,19);
+		StdDraw.setPenColor(washu);
+		StdDraw.filledRectangle(0.5, 0.5, 0.45, 0.25);
+		
+		Color brown = new Color(181,135,117);
+		StdDraw.setPenColor(brown);
+		StdDraw.filledCircle(.5, 0.45, 0.15);
+		StdDraw.filledCircle(.41,.58, .045);
+		StdDraw.filledCircle(.59,.58, .045);
+		
+		StdDraw.setPenColor(Color.BLACK);
+		StdDraw.filledCircle(.45, .50 , .01 );
+		StdDraw.filledCircle(.55, .50 , .01 );
+		StdDraw.filledCircle(.5, 0.45, 0.025);
+		StdDraw.arc(.5, .45, .1, 210, 330 );
 		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -15,10 +15,31 @@ import edu.princeton.cs.introcs.StdDraw;
 public class InterpretDrawingFile {
 
 	public static void main(String[] args) throws FileNotFoundException {
-		JFileChooser chooser = new JFileChooser("resources");
-		chooser.showOpenDialog(null);
-		File f = new File(chooser.getSelectedFile().getPath());
+//		JFileChooser chooser = new JFileChooser("resources");
+//		chooser.showOpenDialog(null);
+		File f = new File("resources/ellipse_purple_outlined.txt");
 		Scanner in = new Scanner(f); //making Scanner with a File
+		
+		String shapeType = in.next();
+		int redComponent = in.nextInt();
+		int greenComponent = in.nextInt();
+		int blueComponent = in.nextInt();
+		boolean isFilled = in.nextBoolean();
+		double parameter1 = in.nextDouble();
+		double parameter2 = in.nextDouble();
+		double parameter3 = in.nextDouble();
+		double parameter4 = in.nextDouble();
+		
+		
+		while(isFilled) {
+			//double x = in.nextDouble();
+			StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+			StdDraw.filledRectangle(parameter1, parameter2, parameter3, parameter4);
+		}
+		while (!isFilled) {
+			StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+			StdDraw.rectangle(parameter1, parameter2, parameter3, parameter4);
+		}
 		
 	}
 }
